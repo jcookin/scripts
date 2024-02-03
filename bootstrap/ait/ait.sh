085aac47941b94c5b956ae60982220b730ae4d14
jcookin <>
2024-02-03 16:45:29 -0700

    Get docker installs working

--- bootstrap/ait/ait.sh
+++ bootstrap/ait/ait.sh
@@ -6,7 +6,6 @@
 # shellcheck source=../config.sh
 source ../config.sh
 
-
 set -x
 
 ## Kernel check
@@ -89,6 +88,9 @@ fi
 pprint_ok "DONE"
 
 ## Install Docker
+pprint_info "Installing Docker..."
+
+
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -99,11 +101,14 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
-if sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; then
+if sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; then
+    # Add user to docker group for non-root user use
+    sudo groupadd docker
+    sudo usermod -aG docker "$USER"
     pprint_ok "Success"
 else
     pprint_err "Error!"
