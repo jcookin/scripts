cf06683698a8a8626565ee80e36dfd0de72efb1c
jcookin <>
2024-02-06 22:41:23 -0700

    Add spotify install

--- bootstrap/ait/ait.sh
+++ bootstrap/ait/ait.sh
@@ -124,4 +124,17 @@ else
     pprint_err "Error! See output"
 fi
 
-pprint_info "NOTICE: Recommend reboot before continuing"
+## Install Spotify
+pprint_info "Installing Spotify..."
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+
+if sudo apt-get install spotify-client; then
+    pprint_ok "Success!"
+else
+    pprint_err "Error!"
+fi
+
+pprint_info "NOTICE: Recommend reboot"
